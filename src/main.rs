cb60372d5c6b05bb77d76a09743f47b633781e61
nyctef <nyctef@nyctef.com>
2023-07-07 10:41:00 +0100

    sketching out code framework
    leaning towards a more OOP like implementation since it could be
    interesting to try something different

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use derive_more::Constructor;
+
 fn main() {
     println!("Hello, world!");
+}
+#[derive(Constructor, Debug)]
+struct Game {}
+impl Game {
+    fn add_player(&mut self, name: &str, player: &mut dyn Player) {
+        todo!()
+    }
+}
+
+trait Player {}
+
+#[derive(Constructor, Debug)]
+struct AlwaysBuyCopper {}
+
+impl Player for AlwaysBuyCopper {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{AlwaysBuyCopper, Game};
+
+    #[test]
+    fn a_game_can_start() {
+        let mut game = Game::new();
+        let mut player_1 = AlwaysBuyCopper::new();
+        game.add_player("Player 1", &mut player_1);
+    }
 }
