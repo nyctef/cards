21da46f1157f4db45715291a795c8d0de9c9d4d5
nyctef <nyctef@nyctef.com>
2023-07-18 21:46:21 +0100

    switch to a faster prng
    since a quick profile showed that StdRng seemed to take up a decent
    chunk of time

--- src/game/shuffler.rs
+++ src/game/shuffler.rs
@@ -1,7 +1,14 @@
 use std::cell::RefCell;
 
 use derive_more::Constructor;
-use rand::{self, rngs::StdRng, Rng, SeedableRng};
+use rand::{self, Rng, SeedableRng};
+// We use Pcg64Mcg over StdRng because it's faster and we don't need cryptographic security.
+// Also referencing Pcg64Mcg directly instead of SmallRng since apparently SmallRng can
+// decide to change implementations, and it might be nice to have reproducible games
+// based on a seed.
+// (Although tbh if we're going to serialize games, we might just want to serialize the set
+// of cards that got drawn rather than the seed used to pick them)
+use rand_pcg::Pcg64Mcg as PRng;
 
 pub trait Shuffler<T>: std::fmt::Debug {
     /**
@@ -12,19 +19,19 @@ pub trait Shuffler<T>: std::fmt::Debug {
 
 #[derive(Debug)]
 pub struct RandomShuffler {
-    rng: RefCell<StdRng>,
+    rng: RefCell<PRng>,
 }
 impl RandomShuffler {
     #[cfg(test)]
     pub fn new(seed: u64) -> Self {
         RandomShuffler {
-            rng: StdRng::seed_from_u64(seed).into(),
+            rng: PRng::seed_from_u64(seed).into(),
         }
     }
 
     pub fn unseeded() -> Self {
         RandomShuffler {
-            rng: StdRng::from_entropy().into(),
+            rng: PRng::from_entropy().into(),
         }
     }
 }
